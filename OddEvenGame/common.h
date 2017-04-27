c5c03dd93b20d1cd63cdffe3a56b0ab49e96aa36
BlockDMask <balxbalta@gmail.com>
2017-04-27 20:27:24 +0900

    1st created

---
+++ OddEvenGame/common.h
@@ -0,0 +1,12 @@
+//
+// Created by BlockDMask on 26/04/2017.
+//
+
+#ifndef ODDEVENGAME_COMMON_H
+#define ODDEVENGAME_COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif //ODDEVENGAME_COMMON_H
---
+++ OddEvenGame/main.c
@@ -0,0 +1,31 @@
+#include "common.h"
+#include "game.h"
+
+int main() {
+    int com;
+    int usr;
+
+    puts("=====GAME START=====");
+    puts("");
+
+    com = ChoiceOfCom();
+    usr = ChoiceOfUsr();
+
+    puts("");
+
+    puts(">>>>>>RESULT>>>>>>");
+    WhoIsWinner(com, usr);
+
+    printf("=> com : %d\n", com);
+
+    if(usr == EVEN)
+        puts("=> usr : EVEN");
+    else
+        puts("=> usr : ODD");
+
+    puts("");
+
+
+
+    return 0;
+}
